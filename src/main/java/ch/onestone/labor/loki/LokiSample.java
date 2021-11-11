d60008fe6cca30632ccd32aca5d8cf05b8d828f8
eiged <dav.eigenheer@gmail.com>
2021-11-11 15:13:05 +0100

    Initial commit

---
+++ src/main/java/ch/onestone/labor/loki/LokiSample.java
@@ -0,0 +1,19 @@
+package ch.onestone.labor.loki;
+
+import java.time.LocalTime;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LokiSample {
+
+	public static void main(String[] args) {
+		log.info("Current time : " + LocalTime.now());
+		log.error("TEST ERROR {}", LokiSample.class.getSimpleName());
+		log.info("hello");
+		log.error("hello");
+		log.debug("hello");
+
+	}
+
+}
